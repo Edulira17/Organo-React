8d3a86b4d8d9f32a1a2c5f26220cf594ed7b117c
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-08-21 14:59:33 -0400

    trabalhando com estados nos inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario/Formulario';
 
 
 function App() {
+  
+  const[colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+
+    setColaboradores([...colaboradores, colaborador])
+  }
 
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
     </div>
   );
 }
