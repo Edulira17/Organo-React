9d33876c7a51349c3d0842a57f09e7288127d9f3
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-08-22 09:09:08 -0400

    add: componente dinamico dos times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
-import Formulario from './components/Formulario/Formulario';
+import Formulario from './components/Formulario/';
+import Time from './components/Time';
 
 
 function App() {
@@ -17,6 +18,13 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
+      <Time nome="Programação"/>
+      <Time nome="FrontEnd"/>
+      <Time nome="Data Science"/>
+      <Time nome="Devops"/>
+      <Time nome="UX e Design"/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação e Gestão"/>
     </div>
   );
 }
